dae0e0b52a2f46ad8336aca024bf637ea064a639
yoonjee <leecss2@naver.com>
2026-08-28 11:35:33 +0900

    Update BaseDashboardCard.vue

--- weather/src/components/exercise/BaseDashboardCard.vue
+++ weather/src/components/exercise/BaseDashboardCard.vue
@@ -1,21 +1,27 @@
 <script setup>
 defineProps({
-  title: String,
+  title: {
+    type: String,
+    default: '',
+  },
 })
 </script>
 
 <template>
-  <div class="dashboard-card">
-    <h3>{{ title }}</h3>
-    <slot></slot>
-  </div>
+  <section class="dashboard-card">
+    <h2 v-if="title" class="dashboard-card__title">{{ title }}</h2>
+    <slot />
+  </section>
 </template>
 
 <style scoped>
 .dashboard-card {
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 16px;
-  margin-bottom: 12px;
+  text-align: center;
+}
+
+.dashboard-card__title {
+  font-size: 1.6rem;
+  font-weight: 800;
+  margin-bottom: 20px;
 }
 </style>
