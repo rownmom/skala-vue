55070293fe7153a4c4d8abbce72ec080b1b4c895
yoonjee <leecss2@naver.com>
2026-08-28 11:41:40 +0900

    Update weatherCities.js

--- weather/src/config/weatherCities.js
+++ weather/src/config/weatherCities.js
@@ -2,4 +2,5 @@ export const CITY_LIST = [
   { id: 'city_01', name: '서울', lat: 37.5665, lon: 126.978 },
   { id: 'city_02', name: '수원', lat: 37.2636, lon: 127.0286 },
   { id: 'city_03', name: '부산', lat: 35.1796, lon: 129.0756 },
+  { id: 'city_04', name: '대전', lat: 36.3504, lon: 127.3845 },
 ]
